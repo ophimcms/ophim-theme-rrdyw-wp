c7ad30daad0674a3fc0f46c74291d09583fb881b
NTBDEV <32400888+binhnguyen1998822@users.noreply.github.com>
2025-07-12 15:06:47 +0700

    fix css & mobile menu

--- assets/statics/js/stui_default.js
+++ assets/statics/js/stui_default.js
@@ -8,14 +8,14 @@ var stui = {
 		domain: document.domain,
 		title: document.title,
 		language: (navigator.browserLanguage || navigator.language).toLowerCase(),
-		canvas: function() {
+		canvas: function () {
 			return !!document.createElement("canvas").getContext
 		}(),
-		useragent: function() {
+		useragent: function () {
 			var a = navigator.userAgent;
 			return {
-				mobile: !! a.match(/AppleWebKit.*Mobile.*/),
-				ios: !! a.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+				mobile: !!a.match(/AppleWebKit.*Mobile.*/),
+				ios: !!a.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
 				android: -1 < a.indexOf("Android") || -1 < a.indexOf("Linux"),
 				iPhone: -1 < a.indexOf("iPhone") || -1 < a.indexOf("Mac"),
 				iPad: -1 < a.indexOf("iPad"),
@@ -28,47 +28,47 @@ var stui = {
 		}()
 	},
 	mobile: {
-		popup: function() {
+		popup: function () {
 			$popblock = $(".popup");
-			$(".open-popup").click(function() {
+			$(".open-popup").click(function () {
 				$popblock.addClass("popup-visible");
 				$("body").append('<div class="mask"></div>');
-				$(".close-popup").click(function() {
+				$(".close-popup").click(function () {
 					$popblock.removeClass("popup-visible");
 					$(".mask").remove();
 					$("body").removeClass("modal-open")
 				});
-				$(".mask").click(function() {
+				$(".mask").click(function () {
 					$popblock.removeClass("popup-visible");
 					$(this).remove();
 					$("body").removeClass("modal-open")
 				})
 			})
 		},
-		slide: function() {
-			$.getScript(SitePath+"statics/js/flickity.pkgd.min.js", function() {
-				$(".type-slide").each(function(a) {
-					$index = $(this).find('.active').index()*1;
-					if($index > 3){
-						$index = $index-3;
-					}else{
+		slide: function () {
+			$.getScript(SitePath + "statics/js/flickity.pkgd.min.js", function () {
+				$(".type-slide").each(function (a) {
+					$index = $(this).find('.active').index() * 1;
+					if ($index > 3) {
+						$index = $index - 3;
+					} else {
 						$index = 0;
 					}
 					$(this).flickity({
 						cellAlign: 'left',
 						freeScroll: true,
 						contain: true,
-						prevNextButtons: false,				
+						prevNextButtons: false,
 						pageDots: false,
 						initialIndex: $index
 					});
 				})
 			})
 		},
-		share: function() {
-			$(".open-share").click(function() {
+		share: function () {
+			$(".open-share").click(function () {
 				stui.browser.useragent.weixin ? $("body").append('<div class="mobile-share share-weixin"></div>') : $("body").append('<div class="mobile-share share-other"></div>');
-				$(".mobile-share").click(function() {
+				$(".mobile-share").click(function () {
 					$(".mobile-share").remove();
 					$("body").removeClass("modal-open")
 				})
@@ -76,40 +76,40 @@ var stui = {
 		}
 	},
 	flickity: {
-		carousel: function() {
-			$.getScript(SitePath+"statics/js/flickity.pkgd.min.js", function() {
+		carousel: function () {
+			$.getScript(SitePath + "statics/js/flickity.pkgd.min.js", function () {
 				$('.carousel_default').flickity({
-				  	cellAlign: 'left',
-				  	contain: true,
-				  	wrapAround: true,
-				  	autoPlay: true,
-				  	prevNextButtons: false
+					cellAlign: 'left',
+					contain: true,
+					wrapAround: true,
+					autoPlay: true,
+					prevNextButtons: false
 				});
 				$('.carousel_wide').flickity({
-				  	cellAlign: 'center',
-				  	contain: true,
-				  	wrapAround: true,
-				  	autoPlay: true,
+					cellAlign: 'center',
+					contain: true,
+					wrapAround: true,
+					autoPlay: true,
 				});
 				$('.carousel_center').flickity({
-				  	cellAlign: 'center',
-				  	contain: true,
-				  	wrapAround: true,
-				  	autoPlay: true,
-				  	prevNextButtons: false
+					cellAlign: 'center',
+					contain: true,
+					wrapAround: true,
+					autoPlay: true,
+					prevNextButtons: false
 				});
 				$('.carousel_right').flickity({
-				  	cellAlign: 'left',
-				  	wrapAround: true,
-				  	contain: true,
-				  	pageDots: false
+					cellAlign: 'left',
+					wrapAround: true,
+					contain: true,
+					pageDots: false
 				});
 			})
 		}
 	},
 	images: {
-		lazyload: function() {
-			$.getScript(SitePath+"statics/js/jquery.lazyload.js", function() {
+		lazyload: function () {
+			$.getScript(SitePath + "statics/js/jquery.lazyload.js", function () {
 				$(".lazyload").lazyload({
 					effect: "fadeIn",
 					threshold: 200,
@@ -118,23 +118,23 @@ var stui = {
 				})
 			})
 		},
-		qrcode: function() {
+		qrcode: function () {
 			$("img.qrcode").attr("src", "" + encodeURIComponent(stui.browser.url) + "")
 		}
 	},
 	common: {
-		bootstrap: function() {
-			$.getScript(SitePath+"statics/js/bootstrap.min.js", function() {
-				$('a[data-toggle="tab"]').on("shown.bs.tab", function(a) {
+		bootstrap: function () {
+			$.getScript(SitePath + "statics/js/bootstrap.min.js", function () {
+				$('a[data-toggle="tab"]').on("shown.bs.tab", function (a) {
 					var b = $(a.target).text();
 					$(a.relatedTarget).text();
 					$("span.active-tab").html(b)
 				})
 			})
 		},
-		headroom: function() {
-			$.getScript(SitePath+"statics/js/headroom.min.js", function() {
-				$("#header-top", function() {
+		headroom: function () {
+			$.getScript(SitePath + "statics/js/headroom.min.js", function () {
+				$("#header-top", function () {
 					(new Headroom(document.querySelector("#header-top"), {
 						tolerance: 5,
 						offset: 205,
@@ -147,20 +147,20 @@ var stui = {
 				});
 			})
 		},
-		collapse: function() {
-			$(".detail").on("click", ".detail-more", function() {
+		collapse: function () {
+			$(".detail").on("click", ".detail-more", function () {
 				$detailContent = $(".detail-content");
 				$detailSketch = $(".detail-sketch");
 				"none" == $detailContent.css("display") ? ($(this).html('Rút gọn <i class="icon iconfont icon-less"></i>'), $detailContent.show(), $detailSketch.hide()) : ($(this).html('Xem thêm <i class="icon iconfont icon-moreunfold"></i>'), $detailContent.hide(), $detailSketch.show())
 			})
 		},
-		scrolltop: function() {
+		scrolltop: function () {
 			var a = $(window);
 			$scrollTopLink = $("a.backtop");
-			a.scroll(function() {
+			a.scroll(function () {
 				500 < $(this).scrollTop() ? $scrollTopLink.css("display", "block") : $scrollTopLink.css("display", "none")
 			});
-			$scrollTopLink.on("click", function() {
+			$scrollTopLink.on("click", function () {
 				$("html, body").animate({
 					scrollTop: 0
 				}, 400);
@@ -169,8 +169,8 @@ var stui = {
 		}
 	}
 };
-$(document).ready(function() {
-	if(stui.browser.useragent.mobile){	
+$(document).ready(function () {
+	if (stui.browser.useragent.mobile) {
 		stui.mobile.slide();
 		stui.mobile.popup();
 		stui.mobile.share();
--- templates/header.php
+++ templates/header.php
@@ -1,49 +1,3 @@
-<header class="stui-header__top clearfix" id="header-top">
-    <div class="container">
-        <div class="row">
-            <div class="stui-header_bd clearfix">
-                <div class="stui-header__logo">
-                    <a class="" href="/"> <?php op_the_logo('max-height:60px;width:100%') ?></a>
-                </div>
-                <div class="stui-header__side">
-                    <div class="stui-header__search">
-                        <form action="/" method="get" name="formsearch" id="formsearch">
-                            <input type="text" id="wd" name="s" class="sin form-control" value="<?php echo "$s"; ?>" onkeyup="fetch()">
-                            <input type="submit" id="searchbutton" value="" class="hide">
-                            <button class="submit" id="submit" onclick="$('#formsearch').submit();">
-                                <i class="icon iconfont icon-search"></i>
-                            </button>
-                        </form>
-                        <div class="" id="result"></div>
-                    </div>
-                </div>
-                <ul class="stui-header__menu type-slide">
-                    <?php
-                    $menu_items = op_get_menu_array('primary-menu');
-                    foreach ($menu_items as $key => $item) : ?>
-                        <?php if (empty($item['children'])) { ?>
-                            <li><a href="<?= $item['url'] ?>"><?= $item['title'] ?></a></li>
-                        <?php } else { ?>
-                            <li class="nav-menu-item dropdown">
-                                <a href="<?= $item['url'] ?>" title="<?= $item['title'] ?>">
-                                    <span class="nav-menu-item-name"><?= $item['title'] ?></span>
-                                </a>
-                                <ul class="dropdown-menu dropdown-content">
-                                    <?php foreach ($item['children'] as $k => $child): ?>
-                                        <li>
-                                            <a class="dropdown-item" href="<?= $child['url'] ?>"
-                                               title="<?= $child['title'] ?>"><?= $child['title'] ?></a>
-                                        </li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            </li>
-                        <?php } ?>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
 <style>
     #result {
         margin-top: 20px;
@@ -93,21 +47,104 @@
     .dropdown-content {
         display: none;
         position: absolute;
-        background-color: #e3cfcf;
+        background-color: #ffffff;
         min-width: 160px;
         box-shadow: 0px 8px 16px 0px rgba(232, 204, 204, 0.2);
         z-index: 1;
+        max-height: 300px;
+        overflow: auto;
     }
 
     .dropdown:hover .dropdown-content {
         display: block;
     }
-
-    .dropdown-menu li:hover {
-        background: #0a5c6f;
-    }
-
-    .dropdown-menu li {
-        padding-left: 10px;
-    }
 </style>
+<header class="stui-header__top clearfix" id="header-top">
+    <div class="container">
+        <div class="row">
+            <div class="stui-header_bd clearfix">
+                <div class="stui-header__logo">
+                    <a class="" href="/">
+                        <?php op_the_logo('max-height:60px') ?>
+                    </a>
+                </div>
+                <div class="stui-header__side">
+                    <div class="stui-header__search">
+                        <form action="/" method="get" name="formsearch" id="formsearch">
+                            <input type="text" id="wd" name="s" class="sin form-control" value="<?php echo "$s"; ?>" autocomplete="off" placeholder="Tìm kiếm..." onkeyup="fetch()">
+                            <input type="submit" id="searchbutton" value="" class="hide">
+                            <button class="submit" id="submit" onclick="$('#formsearch').submit();">
+                                <i class="icon iconfont icon-search"></i>
+                            </button>
+                        </form>
+                        <div class="" id="result"></div>
+                    </div>
+                </div>
+                <ul class="stui-header__menu type-slide">
+                    <?php
+                    $menu_items = op_get_menu_array('primary-menu');
+                    foreach ($menu_items as $key => $item) : ?>
+                        <?php if (empty($item['children'])) { ?>
+                            <li><a href="<?= $item['url'] ?>"><?= $item['title'] ?></a></li>
+                        <?php } else { ?>
+                            <li class="nav-menu-item dropdown has-dropdown">
+                                <a href="<?= $item['url'] ?>" title="<?= $item['title'] ?>">
+                                    <span class="nav-menu-item-name"><?= $item['title'] ?></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-content">
+                                    <?php foreach ($item['children'] as $k => $child): ?>
+                                        <li>
+                                            <a class="dropdown-item" href="<?= $child['url'] ?>"
+                                               title="<?= $child['title'] ?>"><?= $child['title'] ?></a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </li>
+                        <?php } ?>
+                    <?php endforeach; ?>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<script>
+    $(function() {
+        function isMobile() {
+            return window.innerWidth <= 1024;
+        }
+
+        // Xử lý click menu dropdown trên mobile
+        $('.stui-header__menu .dropdown > a').on('click', function(e) {
+            if (isMobile()) {
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+
+                // Xóa dropdown cũ nếu có
+                $('#mobile-dropdown-portal').remove();
+
+                var $parent = $(this).parent();
+                var $dropdown = $parent.find('.dropdown-content').first().clone();
+                var offset = $(this).offset();
+                var height = $(this).outerHeight();
+
+                // Tạo dropdown mới ở body
+                var $portal = $('<ul id=\"mobile-dropdown-portal\" class=\"dropdown-menu dropdown-content\" style=\"display:block; position:fixed; left:' + offset.left + 'px; top:' + (offset.top + height) + 'px; z-index:99999; background:#fff; min-width:160px; box-shadow:0 8px 16px 0 rgba(0,0,0,0.2);\"></ul>');
+                $portal.append($dropdown.children());
+                $('body').append($portal);
+
+                // Đóng khi click ra ngoài
+                setTimeout(function() {
+                    $(document).one('touchstart click', function(ev) {
+                        if (!$(ev.target).closest('#mobile-dropdown-portal').length) {
+                            $('#mobile-dropdown-portal').remove();
+                        }
+                    });
+                }, 10);
+            }
+        });
+    });
+</script>
